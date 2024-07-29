6e76cc28ab88b9685de3ce420470d43377fb371f
Son Goku <you@example.com>
2024-07-29 17:13:54 -0300

    Async

--- 5to_HospeddApp_C#/Hotel.Core/IAdo.cs
+++ 5to_HospeddApp_C#/Hotel.Core/IAdo.cs
@@ -3,26 +3,43 @@ namespace HotelApp.Core;
 public interface IAdo
 {
     List<Hotel> ObtenerHotel();
+    Task<List<Hotel>> ObtenerHotelAsync();
     void AltaHotel(Hotel hotel);
+    Task AltaHotelAsync(Hotel hotel);
     Hotel? ObtenerHotelPorId(ushort IdHotel);
+    Task<Hotel?> ObtenerHotelPorIdAsync(ushort IdHotel);
 
     List<Cliente> ObtenerCliente();
+    Task<List<Cliente>> ObtenerClienteAsync();
     Cliente? ObtenerClientePorCorreoContrasña(string Email, string Contraseña);
+    Task<Cliente?> ObtenerClientePorCorreoContrasñaAsync(string Email, string Contraseña);
     void AltaCliente(Cliente cliente);
+    Task AltaClienteAsync(Cliente cliente);
 
     List<Cama> ObtenerCama();
+    Task<List<Cama>> ObtenerCamaAsync();
     Cama? ObtenerCamaPorId(byte IdCama);
+    Task<Cama?> ObtenerCamaPorIdAsync(byte IdCama);
     void AltaCama(Cama cama);
+    Task AltaCamaAsync(Cama cama);
 
-    List<Cuarto> ObtenerCuarto();
+    Task<List<Cuarto>> ObtenerCuartoAsync();
     Cuarto? ObtenerCuartoPorId(byte IdCuarto);
+    Task<Cuarto?> ObtenerCuartoPorIdAsync(byte IdCuarto);
     void AltaCuarto(Cuarto cuarto);
+    Task AltaCuartoAsync(Cuarto cuarto);
 
     List<Cuarto_Cama> ObtenerCuarto_Cama();
+    Task<List<Cuarto_Cama>> ObtenerCuarto_CamaAsync();
     Cuarto_Cama? ObtenerCuarto_CamaPorIdCuarto(byte IdCuarto);
-    void AltaCuarto_Cama (Cuarto_Cama cuarto_Cama);
+    Task<Cuarto_Cama?> ObtenerCuarto_CamaPorIdCuartoAsync(byte IdCuarto);
+    void AltaCuarto_Cama(Cuarto_Cama cuarto_Cama);
+    Task AltaCuarto_CamaAsync(Cuarto_Cama cuarto_Cama);
 
     List<Hotel_Cuarto> ObtenerHotel_Cuarto();
+    Task<List<Hotel_Cuarto>> ObtenerHotel_CuartoAsync();
     Hotel_Cuarto? ObtenerHotel_CuartoPorId(ushort IdHotel, byte IdCuarto);
-    void AltaHotel_Cuarto (Hotel_Cuarto hotel_Cuarto);
+    Task<Hotel_Cuarto?> ObtenerHotel_CuartoPorIdAsync(ushort IdHotel, byte IdCuarto);
+    void AltaHotel_Cuarto(Hotel_Cuarto hotel_Cuarto);
+    Task AltaHotel_CuartoAsync(Hotel_Cuarto hotel_Cuarto);
 }
--- 5to_HospeddApp_C#/Hotel.Dapper/AdoDapper.cs
+++ 5to_HospeddApp_C#/Hotel.Dapper/AdoDapper.cs
@@ -2,7 +2,6 @@
 using Dapper;
 using MySqlConnector;
 using HotelApp.Core;
-using System.Reflection.Metadata;
 
 namespace HotelApp.Dapper;
 public class AdoDapper : IAdo
@@ -14,20 +13,11 @@ public class AdoDapper : IAdo
 
 
 #region 'Hotel'
-    private readonly string _queryHotel
-        = "SELECT * FROM Hotel";
-    
-    private readonly string _queryHotelPorId
-        = "SELECT * FROM Hotel WHERE IdHotel = @unIdhotel";
-    
-    public List<Hotel> ObtenerHotel() => _conexion.Query<Hotel>(_queryHotel).ToList();
 
-    public Hotel? ObtenerHotelPorId(ushort IdHotel) => 
-    _conexion.QueryFirstOrDefault<Hotel>(_queryHotelPorId, new { unIdhotel = IdHotel });
-
-    public void AltaHotel(Hotel hotel)
+    private DynamicParameters ParametrosAltaHotel (Hotel hotel)
     {
         var parametros = new DynamicParameters();
+
         parametros.Add("@unIdHotel", direction: ParameterDirection.Output);
         parametros.Add("@unNombre", hotel.Nombre);
         parametros.Add("@unDomicilio", hotel.Domicilio);
@@ -35,9 +25,62 @@ public class AdoDapper : IAdo
         parametros.Add("@unContraseña", hotel.Contraseña);
         parametros.Add("@unEstrella", hotel.Estrella);
 
+        return parametros;
+    }
+
+    private readonly string _queryHotel
+        = "SELECT * FROM Hotel";
+    
+    private readonly string _queryHotelPorId
+        = "SELECT * FROM Hotel WHERE IdHotel = @unIdhotel";
+    
+    public List<Hotel> ObtenerHotel() => _conexion.Query<Hotel>(_queryHotel).ToList();
+    
+    public async Task<List<Hotel>> ObtenerHotelAsync()
+    {
+        var hotel = (await _conexion.QueryAsync<Hotel>(_queryHotel)).ToList();
+        return  hotel;
+    }
+
+    public Hotel? ObtenerHotelPorId(ushort IdHotel) => 
+    _conexion.QueryFirstOrDefault<Hotel>(_queryHotelPorId, new { unIdhotel = IdHotel });
+
+    public async Task<Hotel?> ObtenerHotelPorIdAsync(ushort IdHotel)
+    {
+        var hotel = await _conexion.QueryFirstOrDefaultAsync<Hotel>(_queryHotelPorId, new { unIdhotel = IdHotel });
+        return hotel;
+    }
+
+    public void AltaHotel(Hotel hotel)
+    {
+        var parametros = ParametrosAltaHotel(hotel);
         try
         {
             _conexion.Execute("AltaHotel", parametros);
+
+            //Obtengo el valor de parametro de tipo salida
+            hotel.IdHotel = parametros.Get<ushort>("@unIdHotel");
+        }
+        catch (MySqlException error)
+        {
+            if (error.ErrorCode == MySqlErrorCode.DuplicateKeyEntry)
+            {
+                // Verificar si el error es por el Email
+                if (error.Message.Contains("Email"))
+                {
+                    throw new ConstraintException("El Email " + hotel.Email + " ya se encuentra en uso.");
+                }
+            }
+            throw;
+        }
+    }
+
+    public async Task AltaHotelAsync(Hotel hotel)
+    {
+        var parametros = ParametrosAltaHotel(hotel);
+        try
+        {
+            await _conexion.ExecuteAsync("AltaHotel", parametros);
 
             //Obtengo el valor de parametro de tipo salida
             hotel.IdHotel = parametros.Get<ushort>("@unIdHotel");
@@ -59,6 +102,19 @@ public class AdoDapper : IAdo
 
 #region 'Cliente'
 
+    private DynamicParameters ParametrosAltaCliente(Cliente cliente)
+    {
+        var parametros = new DynamicParameters();
+
+        parametros.Add("@unDni", cliente.Dni);
+        parametros.Add("@unNombre", cliente.Nombre);
+        parametros.Add("@unApellido", cliente.Apellido);
+        parametros.Add("@unEmail", cliente.Email);
+        parametros.Add("@unContraseña", cliente.Contraseña);
+
+        return parametros;
+    }
+
     private readonly string _queryCliente
         = "SELECT * FROM Cliente";
     private readonly string _queryClienteCorreoContraseña
@@ -67,22 +123,57 @@ public class AdoDapper : IAdo
         
     public List<Cliente> ObtenerCliente() => _conexion.Query<Cliente>(_queryCliente).ToList();
 
+    public async Task<List<Cliente>> ObtenerClienteAsync()
+    {
+        var cliente = (await _conexion.QueryAsync<Cliente>(_queryCliente)).ToList();
+        return cliente;
+    }
+
     public Cliente? ObtenerClientePorCorreoContrasña(string Email, string Contraseña) => 
     _conexion.QueryFirstOrDefault<Cliente>(_queryClienteCorreoContraseña, new { unEmail = Email, unContraseña = Contraseña });
 
+    public async Task<Cliente?> ObtenerClientePorCorreoContrasñaAsync(string Email, string Contraseña)
+    {
+        var cliente = await _conexion.QueryFirstOrDefaultAsync<Cliente>(_queryClienteCorreoContraseña, new { unEmail = Email, unContraseña = Contraseña });
+        return cliente;
+    }
 
     public void AltaCliente(Cliente cliente)
     {
         //Preparo los parametros del Stored Procedure
-        var parametros = new DynamicParameters();
-        parametros.Add("@unDni", cliente.Dni);
-        parametros.Add("@unNombre", cliente.Nombre);
-        parametros.Add("@unApellido", cliente.Apellido);
-        parametros.Add("@unEmail", cliente.Email);
-        parametros.Add("@unContraseña", cliente.Contraseña);
+        var parametros = ParametrosAltaCliente(cliente);
         try
         {
             _conexion.Execute("RegistrarCliente", parametros);
+
+            //Obtengo el valor de parametro de tipo salida
+            cliente.Dni = parametros.Get<uint>("@unDni");
+        }
+        catch (MySqlException error)
+        {
+            if (error.ErrorCode == MySqlErrorCode.DuplicateKeyEntry)
+            {
+                // Verificar si el error es por el Email o por el Dni
+                if (error.Message.Contains("Email"))
+                {
+                    throw new ConstraintException("El Email " + cliente.Email + " ya se encuentra en uso.");
+                }
+                else if (error.Message.Contains("PRIMARY"))
+                {
+                    throw new ConstraintException("El Dni " + cliente.Dni + " ya se encuentra registrado.");
+                }
+            }
+            throw;
+        }
+    }
+
+    public async Task AltaClienteAsync(Cliente cliente)
+    {
+        //Preparo los parametros del Stored Procedure
+        var parametros = ParametrosAltaCliente(cliente);
+        try
+        {
+            await _conexion.ExecuteAsync("RegistrarCliente", parametros);
 
             //Obtengo el valor de parametro de tipo salida
             cliente.Dni = parametros.Get<uint>("@unDni");
@@ -108,6 +199,17 @@ public class AdoDapper : IAdo
 
 #region 'Cama'
 
+    private DynamicParameters ParametrosAltaCama(Cama cama)
+    {
+        var parametros = new DynamicParameters();
+
+        parametros.Add("@unIdCama", direction: ParameterDirection.Output);
+        parametros.Add("@unNombre", cama.Nombre);
+        parametros.Add("@unPueden_dormir", cama.Pueden_dormir);
+
+        return parametros;
+    }
+
     private readonly string _queryCama
         = "SELECT * FROM Cama";
 
@@ -115,14 +217,24 @@ public class AdoDapper : IAdo
         = "SELECT * FROM Cama WHERE IdCama = @unIdCama";
 
     public List<Cama> ObtenerCama() => _conexion.Query<Cama>(_queryCama).ToList();
+    
+    public async Task<List<Cama>> ObtenerCamaAsync()
+    {
+        var cama = (await _conexion.QueryAsync<Cama>(_queryCama)).ToList();
+        return cama;
+    }
     public Cama? ObtenerCamaPorId(byte IdCama) => 
     _conexion.QueryFirstOrDefault<Cama>(_queryCamaPorId, new { unIdCama = IdCama });
+
+    public async Task<Cama?> ObtenerCamaPorIdAsync(byte IdCama)
+    {
+        var cama = await _conexion.QueryFirstOrDefaultAsync<Cama>(_queryCamaPorId, new { unIdCama = IdCama });
+        return cama;
+    }
+
     public void AltaCama(Cama cama)
     {
-        var parametros = new DynamicParameters();
-        parametros.Add("@unIdCama", direction: ParameterDirection.Output);
-        parametros.Add("@unNombre", cama.Nombre);
-        parametros.Add("@unPueden_dormir", cama.Pueden_dormir);
+        var parametros = ParametrosAltaCama(cama);
 
         try
         {
@@ -145,9 +257,46 @@ public class AdoDapper : IAdo
         }
     }
 
+    public async Task AltaCamaAsync(Cama cama)
+    {
+        var parametros = ParametrosAltaCama(cama);
+
+        try
+        {
+            await _conexion.ExecuteAsync("AltaCama", parametros);
+
+            //Obtengo el valor de parametro de tipo salida
+            cama.IdCama = parametros.Get<byte>("@unIdCama");
+        }
+        catch (MySqlException error)
+        {
+            if (error.ErrorCode == MySqlErrorCode.DuplicateKeyEntry)
+            {
+                // Verificar si el error es por el Nombre
+                if (error.Message.Contains("Nombre"))
+                {
+                    throw new ConstraintException("El nombre " + cama.Nombre + " ya se encuentra en uso.");
+                }
+            }
+            throw;
+        }
+    }
+
     #endregion
 
 #region 'Cuarto'
+
+    private DynamicParameters ParametrosAltaCuarto(Cuarto cuarto)
+    {
+        var parametros = new DynamicParameters();
+
+        parametros.Add("@unIdCuarto", direction: ParameterDirection.Output);
+        parametros.Add("@unCochera", cuarto.Cochera);
+        parametros.Add("@unNoche", cuarto.Noche);
+        parametros.Add("@unDescripcion", cuarto.Descripcion);
+
+        return parametros;
+    }
     private readonly string _queryCuarto
     = "SELECT * FROM Cuarto";
     private readonly string _queryCuartoPorId
@@ -155,15 +304,24 @@ public class AdoDapper : IAdo
     
     public List<Cuarto> ObtenerCuarto() => _conexion.Query<Cuarto>(_queryCuarto).ToList();
 
-    public Cuarto? ObtenerCuartoPorId(byte IdCuarto) => _conexion.QueryFirstOrDefault<Cuarto>(_queryCuartoPorId, new { unIdCuarto = IdCuarto });
+    public async Task<List<Cuarto>> ObtenerCuartoAsync()
+    {
+        var cuarto = (await _conexion.QueryAsync<Cuarto>(_queryCuarto)).ToList();
+        return cuarto;
+    }
+
+    public Cuarto? ObtenerCuartoPorId(byte IdCuarto) 
+    => _conexion.QueryFirstOrDefault<Cuarto>(_queryCuartoPorId, new { unIdCuarto = IdCuarto });
+
+    public async Task<Cuarto?> ObtenerCuartoPorIdAsync(byte IdCuarto)
+    {
+        var cuarto = await _conexion.QueryFirstOrDefaultAsync<Cuarto>(_queryCuartoPorId, new { unIdCuarto = IdCuarto });
+        return cuarto;
+    }
 
     public void AltaCuarto(Cuarto cuarto)
     {
-        var parametros = new DynamicParameters();
-        parametros.Add("@unIdCuarto", direction: ParameterDirection.Output);
-        parametros.Add("@unCochera", cuarto.Cochera);
-        parametros.Add("@unNoche", cuarto.Noche);
-        parametros.Add("@unDescripcion", cuarto.Descripcion);
+        var parametros = ParametrosAltaCuarto(cuarto);
 
         try
         {
@@ -185,25 +343,71 @@ public class AdoDapper : IAdo
         }
     }
 
+    public async Task AltaCuartoAsync(Cuarto cuarto)
+    {
+        var parametros = ParametrosAltaCuarto(cuarto);
+
+        try
+        {
+            await _conexion.ExecuteAsync("AltaCuarto", parametros);
+
+            //Obtengo el valor de parametro de tipo salida
+            cuarto.IdCuarto = parametros.Get<byte>("@unIdCuarto");
+        }
+        catch (MySqlException error)
+        {
+            if (error.ErrorCode == MySqlErrorCode.DuplicateKeyEntry)
+            {
+                if (error.Message.Contains("Descripcion"))
+                {
+                    throw new ConstraintException("La Descripcion " + cuarto.Descripcion + " ya se encuentra en uso.");
+                }
+            }
+            throw;
+        }
+    }
+
     #endregion
 
 #region 'Cuarto_Cama'
+
+    private DynamicParameters ParametrosAltaCuarto_Cama(Cuarto_Cama cuarto_Cama)
+    {
+        var parametros = new DynamicParameters();
+
+        parametros.Add("@unIdCuarto", cuarto_Cama.IdCuarto);
+        parametros.Add("@unIdCama", cuarto_Cama.IdCama);
+        parametros.Add("@unCantidad_de_cama", cuarto_Cama.Cantidad_de_cama);
+
+        return parametros;
+    }
 
     private readonly string _queryCuarto_Cama 
     = "SELECT * FROM Cuarto_Cama";
     private readonly string _queryCuarto_CamaPorIdCuarto
     = "SELECT * FROM Cuarto_Cama WHERE IdCuarto = @unIdCuarto";
 
-    public List<Cuarto_Cama> ObtenerCuarto_Cama() => _conexion.Query<Cuarto_Cama>(_queryCuarto_Cama).ToList();
+    public List<Cuarto_Cama> ObtenerCuarto_Cama() => 
+    _conexion.Query<Cuarto_Cama>(_queryCuarto_Cama).ToList();
 
-    public Cuarto_Cama? ObtenerCuarto_CamaPorIdCuarto(byte IdCuarto) => _conexion.QueryFirstOrDefault<Cuarto_Cama>(_queryCuarto_CamaPorIdCuarto, new { unIdCuarto = IdCuarto });
+    public async Task<List<Cuarto_Cama>> ObtenerCuarto_CamaAsync()
+    {
+        var cuarto_Cama = (await _conexion.QueryAsync<Cuarto_Cama>(_queryCuarto_Cama)).ToList();
+        return cuarto_Cama;
+    }
+
+    public Cuarto_Cama? ObtenerCuarto_CamaPorIdCuarto(byte IdCuarto) =>
+    _conexion.QueryFirstOrDefault<Cuarto_Cama>(_queryCuarto_CamaPorIdCuarto, new { unIdCuarto = IdCuarto });
     
+    public async Task<Cuarto_Cama?> ObtenerCuarto_CamaPorIdCuartoAsync(byte IdCuarto)
+    {
+        var cuarto_Cama = await _conexion.QueryFirstOrDefaultAsync<Cuarto_Cama>(_queryCuarto_CamaPorIdCuarto, new { unIdCuarto = IdCuarto });
+        return cuarto_Cama;
+    }
+
     public void AltaCuarto_Cama(Cuarto_Cama cuarto_Cama) 
     {
-        var parametros = new DynamicParameters();
-        parametros.Add("@unIdCuarto", cuarto_Cama.IdCuarto);
-        parametros.Add("@unIdCama", cuarto_Cama.IdCama);
-        parametros.Add("@unCantidad_de_cama", cuarto_Cama.Cantidad_de_cama);
+        var parametros = ParametrosAltaCuarto_Cama(cuarto_Cama);
 
         try
         {
@@ -226,22 +430,71 @@ public class AdoDapper : IAdo
         }
     }
 
+    public async Task AltaCuarto_CamaAsync(Cuarto_Cama cuarto_Cama) 
+    {
+        var parametros = ParametrosAltaCuarto_Cama(cuarto_Cama);
+
+        try
+        {
+            await _conexion.ExecuteAsync("AltaCuarto_Cama", parametros);
+
+            //Obtengo el valor de parametro de tipo salida
+            cuarto_Cama.IdCuarto = parametros.Get<byte>("@unIdCuarto");
+            cuarto_Cama.IdCama = parametros.Get<byte>("@unIdCama");
+        }
+        catch (MySqlException error)
+        {
+            if (error.ErrorCode == MySqlErrorCode.DuplicateKeyEntry)
+            {
+                if (error.Message.Contains("PRIMARY"))
+                {
+                    throw new ConstraintException("El IdCuarto " + cuarto_Cama.IdCuarto + " y el IdCama " + cuarto_Cama.IdCama + " ya se encuentra en uso.");
+                }
+            }
+            throw;
+        }
+    }
+
     #endregion
 
 #region 'Hotel_Cuarto'
+
+    private DynamicParameters ParametrosAltaHotel_Cuarto(Hotel_Cuarto hotel_Cuarto)
+    {
+        var parametros = new DynamicParameters();
+
+        parametros.Add("@unIdHotel", hotel_Cuarto.IdHotel);
+        parametros.Add("@unIdCuarto", hotel_Cuarto.IdCuarto);
+        parametros.Add("@unPrecio", hotel_Cuarto.Numero);
+
+        return parametros;
+    }
 
     private readonly string _queryHotel_Cuarto
     = "SELECT * FROM Hotel_Cuarto";
     private readonly string _queryHotel_CuartoPorId
     = "SELECT * FROM Hotel_Cuarto WHERE IdHotel = @unIdHotel";
-    public List<Hotel_Cuarto> ObtenerHotel_Cuarto() => _conexion.Query<Hotel_Cuarto>(_queryHotel_Cuarto).ToList();
-    public Hotel_Cuarto? ObtenerHotel_CuartoPorId(ushort IdHotel, byte IdCuarto) => _conexion.QueryFirstOrDefault<Hotel_Cuarto>(_queryHotel_CuartoPorId, new { unIdHotel = IdHotel, unIdCuarto = IdCuarto });
+    public List<Hotel_Cuarto> ObtenerHotel_Cuarto() => 
+    _conexion.Query<Hotel_Cuarto>(_queryHotel_Cuarto).ToList();
+
+    public async Task<List<Hotel_Cuarto>> ObtenerHotel_CuartoAsync()
+    {
+        var hotel_Cuarto = (await _conexion.QueryAsync<Hotel_Cuarto>(_queryHotel_Cuarto)).ToList();
+        return hotel_Cuarto;
+    }
+
+    public Hotel_Cuarto? ObtenerHotel_CuartoPorId(ushort IdHotel, byte IdCuarto) 
+    => _conexion.QueryFirstOrDefault<Hotel_Cuarto>(_queryHotel_CuartoPorId, new { unIdHotel = IdHotel, unIdCuarto = IdCuarto });
+    
+    public async Task<Hotel_Cuarto?> ObtenerHotel_CuartoPorIdAsync(ushort IdHotel, byte IdCuarto)
+    {
+        var hotel_Cuarto = await _conexion.QueryFirstOrDefaultAsync<Hotel_Cuarto>(_queryHotel_CuartoPorId, new { unIdHotel = IdHotel, unIdCuarto = IdCuarto });
+        return hotel_Cuarto;
+    }
+
     public void AltaHotel_Cuarto (Hotel_Cuarto hotel_Cuarto)
     {
-        var parametros = new DynamicParameters();
-        parametros.Add("@unIdHotel", hotel_Cuarto.IdHotel);
-        parametros.Add("@unIdCuarto", hotel_Cuarto.IdCuarto);
-        parametros.Add("@unPrecio", hotel_Cuarto.Numero);
+        var parametros = ParametrosAltaHotel_Cuarto(hotel_Cuarto);
 
         try
         {
@@ -258,10 +511,30 @@ public class AdoDapper : IAdo
             }
         }
     }
-#endregion
 
-#region 'Reserva'
-    
-#endregion
+    public async Task AltaHotel_CuartoAsync(Hotel_Cuarto hotel_Cuarto)
+    {
+        var parametros = ParametrosAltaHotel_Cuarto(hotel_Cuarto);
+
+        try
+        {
+            await _conexion.ExecuteAsync("AltaHotel_Cuarto", parametros);
+        }
+        catch (MySqlException error)
+        {
+            if(error.ErrorCode == MySqlErrorCode.DuplicateKeyEntry)
+            {
+                if(error.Message.Contains("PRIMARY"))
+                {
+                    throw new ConstraintException("El IdHotel " + hotel_Cuarto.IdHotel + " y el IdCuarto " + hotel_Cuarto.IdCuarto + " ya se encuentra en uso.");
+                }
+            }
+        }
+    }
+    #endregion
+
+    #region 'Reserva'
+
+    #endregion
 
 }
